3bb2cc580be18d02ed93409588599ea37e3286cd
shitao <133397418@qq.com>
2024-11-07 10:43:14 +0800

    ✨ feat(utils): 添加进度处理功能
    - 引入新的`ProgressHandler`类来管理进度显示
    - 使用VS Code的进度API进行进度更新和取消操作支持

--- src/utils.ts
+++ src/utils.ts
@@ -1,40 +1,22 @@
 import * as vscode from 'vscode';
 
 /**
- * Waits for a specified amount of time before resolving the promise.
- * 
- * @param {number} timeMs - The time in milliseconds to wait before resolving the promise.
- * @returns {Promise<number>} - A promise that resolves after the specified time.
+ * Adds progress handling functionality.
  */
-export async function wait(timeMs = 300) {
-  return new Promise((resolve) => setTimeout(() => resolve(1), timeMs));
-}
-
-/**
- * Displays an information message in the VS Code window.
- * 
- * @param {string} message - The message to display.
- */
-export async function infoMsg(message: string) {
-  vscode.window.showInformationMessage(`[ai-commit] ${message}`);
-}
-
-/**
- * Displays an error message in the VS Code window.
- * 
- * @param {string} msg - The error message.
- * @param {any} err - The error object.
- */
-export async function errMsg(msg: string, err: any) {
-  vscode.window.showErrorMessage(`[ai-commit] ${msg}, Error: ${err.message}`);
-}
-
-/**
- * Removes conventional commit words from a message.
- * 
- * @param {string} message - The message to modify.
- * @returns {string} - The modified message.
- */
-export function removeConventionalCommitWord(message: string): string {
-  return message.replace(/^(fix|feat)\((.+?)\):/, '($2):');
+export class ProgressHandler {
+  static async withProgress<T>(
+    title: string,
+    task: (
+      progress: vscode.Progress<{ message?: string; increment?: number }>
+    ) => Promise<T>
+  ): Promise<T> {
+    return vscode.window.withProgress(
+      {
+        location: vscode.ProgressLocation.Notification,
+        title: `[ai-commit] ${title}`,
+        cancellable: true
+      },
+      task
+    );
+  }
 }
